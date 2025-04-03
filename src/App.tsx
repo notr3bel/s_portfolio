ada2d52f7b86f3ee31d3c3f92ef6f1c4b5217fee
notr3bel <harshathr07@gmail.com>
2025-04-03 12:36:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Menu, X, Play, Youtube, Timer as Vimeo, Instagram, Mail, ChevronDown } from 'lucide-react';
 import { useState, useEffect } from 'react';
-
+import t3 from "./assets/images/3.jpg"
 function App() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [activeSection, setActiveSection] = useState('home');
@@ -30,7 +30,7 @@ function App() {
     },
     {
       title: "Mysore",
-      thumbnail: "3.jpg",
+      thumbnail: {t3},
       videoUrl: "https://drive.google.com/file/d/1-ZupMZVvxE7VtmUVSJrjcGukuO1r1Jvw/view?usp=drivesdk",
       category: "Music Video"
     }
